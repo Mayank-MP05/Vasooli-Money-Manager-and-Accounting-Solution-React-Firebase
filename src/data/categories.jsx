939d8ca74bfd5223a5ac7d134bc2772207b1cc2d
Mayank_MP5 <mayank5pande@gmail.com>
2020-07-18 17:01:17 +0530

    Categories Added

---
+++ src/data/categories.jsx
@@ -0,0 +1,69 @@
+export default categories = [
+  {
+    iconclass: "fa fa-credit-card",
+    title: "Bills",
+    description:
+      "Includes electricity bill, water bill, Gas bill and all other monthly bills ",
+  },
+
+  {
+    iconclass: "fa fa-building",
+    title: "EMI",
+    description:
+      "Includes EMI of Home Car or anything on amazon whose premium needs to be paid ",
+  },
+  {
+    iconclass: "fa fa-youtube-play",
+    title: "Entertainment",
+    description:
+      "Includes movie tickets some expensive experience , Amusement park tickets fair ",
+  },
+  {
+    iconclass: "fa fa-cutlery",
+    title: "Food and Drinks",
+    description:
+      "Eating out in Restrarent Street Food Night Life and many such Foodie things",
+  },
+
+  {
+    iconclass: "fa fa-car",
+    title: "Fuel",
+    description: "Petrol Disel or Gasoline Pays for Cars",
+  },
+
+  {
+    iconclass: "fa fa-shopping-cart",
+    title: "Groceries",
+    description:
+      "Dmart Shopping for Pulses , Spices or other cooking related things",
+  },
+  {
+    iconclass: "fa fa-heartbeat",
+    title: "Health",
+    description: "Pays to Doctors , Medical Shops ",
+  },
+  {
+    iconclass: "fa fa-line-chart",
+    title: "Investment",
+    description:
+      "Pay cut for Investment in Stocks, Fixed Deposit, Mutual Funds",
+  },
+
+  {
+    iconclass: "fa fa-cart-plus",
+    title: "Shopping",
+    description: "General Shopping at Malls or Stores",
+  },
+
+  {
+    iconclass: "fa fa-taxi",
+    title: "Travel",
+    description:
+      "For Work or For Picnic all the pays to travelling from one place to the another",
+  },
+  {
+    iconclass: "fa fa-list",
+    title: "Other",
+    description: "All the other pays - Blah Blah Blah",
+  },
+];
